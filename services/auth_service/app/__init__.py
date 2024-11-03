1c00a56a05495d3bc3dace66c51111c6f7df43c8
acheampe <acheampe@oregonstate.edu>
2024-11-02 20:14:32 -0400

    enabling CORS

--- services/auth_service/app/__init__.py
+++ services/auth_service/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app) # Enable CORS for all routes
     app.config.from_object(Config)
 
     # Initialize extensions
